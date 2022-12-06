0aad1b795e50532b0f377c4cf9a5c387ed2258a3
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-06 13:59:15 +0900

    feat: JwtTokenProvider 를 DI 받아서 토큰을 생성하고, 일련의 과정을 거쳐 검증된 토큰을 SecurityContextHolder 에 저장하는 JwtAuthenticationFilter 생성

---
+++ src/main/java/com/example/springsecurityjwt/configuration/JwtAuthenticationFilter.java
@@ -0,0 +1,24 @@
+package com.example.springsecurityjwt.configuration;
+
+import com.example.springsecurityjwt.service.UserService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+
+    }
+}
