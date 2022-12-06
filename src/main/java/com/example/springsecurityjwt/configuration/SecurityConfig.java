e340f5062688735963ecbc1a96f7662dd778607b
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-06 17:15:26 +0900

    feat: 보안 필터 설정 중 cors() 추가 및 권한 허용 및 인증 필요한 URL 패턴 추가

--- src/main/java/com/example/springsecurityjwt/configuration/SecurityConfig.java
+++ src/main/java/com/example/springsecurityjwt/configuration/SecurityConfig.java
@@ -13,10 +13,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@EnableWebSecurity
+@EnableWebSecurity // 이 것을 추가하는 순간 다른 설정이 없으면 API 가 필터를 통과하지 못하게 되면서 호출이 안된다.
 @Configuration
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class SecurityConfig { // extends 하는 방법이 있었는데, 이제는 @Bean 을 사용하는 방식으로 바뀌었다.
 
     private final JwtTokenProvider jwtTokenProvider;
 
@@ -28,12 +28,13 @@ public class SecurityConfig {
         return httpSecurity
                 .httpBasic().disable() // 기본 값인 UI 사용을 하지 않도록 설정합니다.
                 .csrf().disable() // REST API 에서는 브라우저 사용 환경이 아니므로 csrf 보안이 필요치 않다. 비활성합니다.
+                .cors().and() // 이거는 할지 안할지 모르겠다...
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않고 JWT 인증 방식이므로 stateless 입니다.
                 .and()
                 .authorizeRequests() // 사용 권한을 확인한다는 의미입니다.
-                .antMatchers("api/v1/jwt/login").permitAll() // 어떤 URL 의 요청을 허용할 것인지를 명시합니다.
-                .antMatchers(HttpMethod.POST).authenticated() // POST 요청들은 모두 인증을 거쳐야 함을 명시합니다.
-                .anyRequest().hasRole("ADMIN") // 명시되지 않은 기타 요청은 역할을 통해 권한을 확인합니다.
+                .antMatchers("api/v1/jwt/**", "**").permitAll() // 어떤 URL 의 요청을 허용할 것인지를 명시합니다. 로그인은 인증 전에 발생하므로 열어줍니다.
+                .antMatchers("api/v1/comment/new").authenticated() // POST 요청들은 모두 인증을 거쳐야 함을 명시합니다. 회원가입/로그인 빼고는 다 막음.
+                .anyRequest().hasRole("ADMIN") // 명시되지 않은 기타 요청은 역할을 통해 권한을 확인합니다. 관리자는 인증 필요 없음.
                 .and()
                 .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler()) // 접근이 거부될 때는 접근 거부 예외로 처리합니다.
                 .and()
