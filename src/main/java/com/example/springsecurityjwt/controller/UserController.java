f5295f5a0552f4b78993e09c222a74d4d227219a
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-06 15:13:32 +0900

    feat: UserController 에 UserService DI 후 사용자 인증해서 토큰 반환하는 API 구현

--- src/main/java/com/example/springsecurityjwt/controller/UserController.java
+++ src/main/java/com/example/springsecurityjwt/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.springsecurityjwt.controller;
 
 import com.example.springsecurityjwt.domain.Response;
 import com.example.springsecurityjwt.domain.dto.UserLoginRequest;
+import com.example.springsecurityjwt.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/jwt")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
 
     @PostMapping("/login")
     public Response<String> login(@RequestBody UserLoginRequest userLoginRequest) {
-        return Response.success("");
+        String token = userService.authenticate(userLoginRequest.getUsername());
+        return Response.success(token);
     }
 
 }
