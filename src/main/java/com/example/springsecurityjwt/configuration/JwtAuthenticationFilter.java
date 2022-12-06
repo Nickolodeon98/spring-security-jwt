7100e9f5ff3283796351aaaf076b732c5dc47125
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-06 17:13:28 +0900

    feat: 토큰이 없거나 헤더가 없을 때 로그에 에러 상태를 찍기 위해서 토큰 필터에 @Slf4j 추가 후 로그에 에러 내용 출력

--- src/main/java/com/example/springsecurityjwt/configuration/JwtAuthenticationFilter.java
+++ src/main/java/com/example/springsecurityjwt/configuration/JwtAuthenticationFilter.java
@@ -3,6 +3,8 @@ package com.example.springsecurityjwt.configuration;
 import com.example.springsecurityjwt.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -14,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
@@ -24,17 +27,24 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION); // 요청에서 헤더로부터 토큰을 가져오기 위해 헤더를 불러온다.ㅓ
 
         if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
-            filterChain.doFilter(request, response); // 서블릿을 실행하는 메서드. 없음 바로 서블릿 실행 ㄱㄱ;;
+            log.error("Authorization 이 없습니다.");
+            filterChain.doFilter(request, response);
+            return;
         }
 
         String token = null;
         try {
             token = authorizationHeader.split(" ")[1];
         } catch (Exception e) {
+            log.error("토큰이 없습니다.");
             throw new RuntimeException(e);
         }
 
-        if (!jwtTokenProvider.validateToken(token)) filterChain.doFilter(request, response);  // 토큰을 찾았다. 이제 이 토큰이 유효한지 확인해보자.
+        if (!jwtTokenProvider.validateToken(token)) {
+            log.error("토큰이 유효하지 않습니다.");
+            filterChain.doFilter(request, response);  // 토큰을 찾았다. 이제 이 토큰이 유효한지 확인해보자.
+            return;
+        }
 
 
         /* 토큰 존재 여부와 유효성의 검사를 마친 뒤에 SecurityContextHolder 에 인증 정보를 저장한다. */
