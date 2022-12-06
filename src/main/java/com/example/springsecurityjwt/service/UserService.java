a651ff6975fd6e01272451c2829b8a26798925a4
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-06 15:13:00 +0900

    feat: UserService 에 리포지토리 DI

--- src/main/java/com/example/springsecurityjwt/service/UserService.java
+++ src/main/java/com/example/springsecurityjwt/service/UserService.java
@@ -1,21 +1,29 @@
 package com.example.springsecurityjwt.service;
 
 import com.example.springsecurityjwt.configuration.JwtTokenProvider;
+import com.example.springsecurityjwt.domain.entity.User;
 import com.example.springsecurityjwt.domain.entity.UserRole;
+import com.example.springsecurityjwt.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository userRepository;
     private final JwtTokenProvider jwtTokenProvider;
 
-    public String authenticate(String username, String password) {
 
+    public UserDetails loadUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public String authenticate(String username) {
+//        UserDetails user = userRepository.findByUsername(username);
         UserRole role = UserRole.USER;
 
-        String token = jwtTokenProvider.createToken(username, role);
-        return token;
+        return jwtTokenProvider.createToken(username, role);
     }
 }
