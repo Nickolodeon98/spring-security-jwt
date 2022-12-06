ed3dfb1e27cbb5292578379a430ba3d9575e2619
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-06 15:15:42 +0900

    feat: 토큰 필터에서 토큰에 대한 존재 여부와 유효성 검사 후의 동작을 구현
     - SecurityContextHolder 에 인증 정보를 저장하던지
     - 서블릿으로 바로 넘긴다

--- src/main/java/com/example/springsecurityjwt/configuration/JwtAuthenticationFilter.java
+++ src/main/java/com/example/springsecurityjwt/configuration/JwtAuthenticationFilter.java
@@ -3,6 +3,8 @@ package com.example.springsecurityjwt.configuration;
 import com.example.springsecurityjwt.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -19,6 +21,24 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        final String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION); // 요청에서 헤더로부터 토큰을 가져오기 위해 헤더를 불러온다.ㅓ
 
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response); // 서블릿을 실행하는 메서드. 없음 바로 서블릿 실행 ㄱㄱ;;
+        }
+
+        String token = null;
+        try {
+            token = authorizationHeader.split(" ")[1];
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        if (!jwtTokenProvider.validateToken(token)) filterChain.doFilter(request, response);  // 토큰을 찾았다. 이제 이 토큰이 유효한지 확인해보자.
+
+
+        /* 토큰 존재 여부와 유효성의 검사를 마친 뒤에 SecurityContextHolder 에 인증 정보를 저장한다. */
+        SecurityContextHolder.getContext().setAuthentication(jwtTokenProvider.getAuthentication(token));
+        filterChain.doFilter(request, response);
     }
 }
